5be5c65086606230c4cc71045f6204a959ec4080
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-09-26 10:28:08 +0530

    by malik

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,29 +24,7 @@ import SupportModal from "./Pages/Support";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "./Redux/Reducer/authReducer";
 
-// ============================================================
-// COOKIE HELPERS
-// ============================================================
-const setCookie = (name, value, days = 7) => {
-  const expires = new Date();
-  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;SameSite=Lax`;
-};
 
-const getCookie = (name) => {
-  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-  return match ? match[2] : null;
-};
-
-// ============================================================
-// SAVE TOKEN BEFORE APP RENDERS (runs once, synchronously)
-// ============================================================
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhYjY0YjM0YjQ2MzAxODgzMjkyMjQ4YiIsInVzZXJJZCI6MTAwMDE3LCJuYW1lIjoidmlrcmFtIiwiZW1haWwiOiJ2aWtyYW1AZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3OTAzMzYzNDcsImV4cCI6MTc5MDk0MTE0N30.AVKNlCSrk9fRCtYPEqG4hzDC14iHdBlJfLjZ_vGb62s";
-
-if (!getCookie("powerhit")) {
-  setCookie("powerhit", TOKEN, 7);
-}
 
 function App() {
   const dispatch = useDispatch();
